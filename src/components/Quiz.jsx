60bb4bab0f11bd2449a9ea82c1091a40ed665e79
rodhis <rodrigonoliva@gmail.com>
2023-12-15 12:36:02 -0300

    new components to fix timer bugs

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,9 @@
 import { useState, useCallback } from "react";
 
 import QUESTIONS from '../questions.js'
-import QuestionTimer from "./QuestionTimer.jsx";
+import Question from "./Question.jsx";
 import quizCompleteImg from '../assets/quiz-complete.png'
+
 
 export default function Quiz() {
     const [answerState, setAnswerState] = useState('')
@@ -43,40 +44,17 @@ export default function Quiz() {
         )
     }
 
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
-    shuffledAnswers.sort(() => Math.random() - 0.5 )
-
     return (
         <article id="quiz">
-            <session id='question'>
-                <QuestionTimer
-                key={activeQuestionIndex} //esta key serve para destruir e remontar o componente quando muda, o que permite o timer ser resetado.
-                timeout={10000} 
-                onTimeout={handleSkipAnswer}/>
-                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-                <ul id="answers">
-                    {shuffledAnswers.map((answer) => {
-                        const isSelected = userAnswers[userAnswers.length - 1] === answer
-                        let cssClass = ''
-
-                        if (answerState == 'answered' && isSelected) {
-                            cssClass = 'selected'
-                        }
-
-                        if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
-                            cssClass = answerState
-                        }
-                         return ( 
-                            <li key={answer} className="answer">
-                           <button onClick={() => handleSelectAnswer(answer)}
-                           className={cssClass}
-                           >{answer}</button>
-                            </li>
-                         )
-                        }
-                    )}
-                </ul>
-            </session>
+           <Question
+           key={activeQuestionIndex} //esta key serve para destruir e remontar o componente quando muda, o que permite o timer ser resetado.
+           questionText={QUESTIONS[activeQuestionIndex].text}
+           answers={QUESTIONS[activeQuestionIndex].answers}
+           answerState={answerState}
+           selectedAnswer={userAnswers[userAnswers.length - 1]}
+           onSelectAnswer={handleSelectAnswer}
+           onSkipAnswer={handleSkipAnswer}
+           />
         </article>
     )
 }
