d7c4e71ef5ad9647b25eae715d7f2c4e3b48bbe1
rodhis <rodrigonoliva@gmail.com>
2023-12-15 11:36:00 -0300

    timers and initial answers logic

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react"
+
+export default function QuestionTimer({ timeout, onTimeout }) {
+
+    const [remainingTime, setRemainingTime] = useState(timeout)
+
+    useEffect(() => {
+       const timer = setTimeout(onTimeout, timeout)
+
+       return () => {
+        clearTimeout(timer) //effect cleanup function
+       }
+    }, [timeout, onTimeout])
+    
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setRemainingTime(previousRemainingTime => previousRemainingTime - 100)
+        }, 100)
+
+        return () => {  //effect cleanup function, so this useEffect won't get executed twice
+            clearInterval(interval)
+        }
+    }, [])
+
+    return (
+        <progress id="question=time" max={timeout} value={remainingTime}/>
+    )
+}
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,20 +1,38 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 import QUESTIONS from '../questions.js'
+import QuestionTimer from "./QuestionTimer.jsx";
 import quizCompleteImg from '../assets/quiz-complete.png'
 
 export default function Quiz() {
+    const [answerState, setAnswerState] = useState('')
     const [userAnswers, setUserAnswers] = useState([])
 
-    const activeQuestionIndex = userAnswers.length
-
+    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length -1
     const quisIsComplete = activeQuestionIndex === QUESTIONS.length
 
-    function handleSelectAnswer(selectedAnswer) {
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
+        setAnswerState('answered')
         setUserAnswers((previousUserAnswers) => {
             return [...previousUserAnswers, selectedAnswer]
         })
-    }
+
+        setTimeout(() => {
+            if(selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+                setAnswerState('correct')
+            } else {
+                setAnswerState('wrong')
+            }
+
+            setTimeout(() => {
+                setAnswerState('')
+            }, 2000)
+
+        }, 1500)
+
+    }, [activeQuestionIndex])
+
+    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
 
     if(quisIsComplete) {
         return (
@@ -31,13 +49,32 @@ export default function Quiz() {
     return (
         <article id="quiz">
             <session id='question'>
+                <QuestionTimer
+                key={activeQuestionIndex} //esta key serve para destruir e remontar o componente quando muda, o que permite o timer ser resetado.
+                timeout={10000} 
+                onTimeout={handleSkipAnswer}/>
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {shuffledAnswers.map((answer) => (
-                        <li key={answer} className="answer">
-                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-                        </li>
-                    ))}
+                    {shuffledAnswers.map((answer) => {
+                        const isSelected = userAnswers[userAnswers.length - 1] === answer
+                        let cssClass = ''
+
+                        if (answerState == 'answered' && isSelected) {
+                            cssClass = 'selected'
+                        }
+
+                        if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
+                            cssClass = answerState
+                        }
+                         return ( 
+                            <li key={answer} className="answer">
+                           <button onClick={() => handleSelectAnswer(answer)}
+                           className={cssClass}
+                           >{answer}</button>
+                            </li>
+                         )
+                        }
+                    )}
                 </ul>
             </session>
         </article>
